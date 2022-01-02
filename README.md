9134b3abbc97b89d8fa5c547523fd5ca1c06e3ce
M4thez <namemat@gmail.com>
2022-01-02 22:25:34 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# nasa-website
+ 
---
+++ main.js
@@ -0,0 +1,43 @@
+import "./style.css";
+
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector("#bg"),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const material = new THREE.MeshBasicMaterial({
+  color: 0x22bbaa,
+  wireframe: true,
+});
+const torus = new THREE.Mesh(geometry, material);
+
+scene.add(torus);
+
+camera.position.z = 20;
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  torus.rotation.x += 0.005;
+
+  controls.update();
+
+  renderer.render(scene, camera);
+}
+animate();
